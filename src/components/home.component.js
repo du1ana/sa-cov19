84bed8129e3968e0c8bf66c822e79f162439fc4f
DulanaPeiris <dulthar@gmail.com>
2021-08-15 17:48:09 +0530

    getData optimization

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -26,7 +26,6 @@ export default class Home extends Component {
         axios.get('https://covid.ourworldindata.org/data/owid-covid-data.json')
         .then((response) => response.data)
         .then((data) => {
-            console.log(data);
           if(data){
             //sa_data: South asian data
               var sa_data = [];
@@ -45,14 +44,16 @@ export default class Home extends Component {
               sa_data.forEach(country=>{
                 let filtered_country=[];
                 console.log(country);
-                for(let i=0;i<country.length;i++){
-                    if((Date.parse(country[i].date)>=Date.parse('2020-03-08'))){
+                var startidx=0;
+                for(let j=0;j<country.length;j++){
+                  if((Date.parse(country[j].date)<Date.parse('2020-03-08'))){
+                    startidx++;
+                  }
+                }
+                for(let i=startidx;i<country.length;i++){
                       if(country[i].new_cases_smoothed_per_million<0)
                         country[i].new_cases_smoothed_per_million=0;
-                      if(country[i].new_cases<0)
-                        country[i].new_cases=0;
                       filtered_country.push(country[i])
-                    }
                 }
                 sa_data_filtered.push(filtered_country);
               })
@@ -65,32 +66,24 @@ export default class Home extends Component {
                 lka_new_cases_smoothed_per_million=[],
                 lka_new_deaths_smoothed_per_million=[],
                 lka_total_cases_per_million=[],
-                lka_new_cases=[],
                 afg_new_cases_smoothed_per_million=[],
                 afg_new_deaths_smoothed_per_million=[],
                 afg_total_cases_per_million=[],
-                afg_new_cases=[],
                 bgd_new_cases_smoothed_per_million=[],
                 bgd_total_cases_per_million=[],
-                bgd_new_cases=[],
                 btn_new_cases_smoothed_per_million=[],
                 btn_total_cases_per_million=[],
-                btn_new_cases=[],
                 ind_new_cases_smoothed_per_million=[],
                 ind_new_deaths_smoothed_per_million=[],
                 ind_total_cases_per_million=[],
-                ind_new_cases=[],
                 mdv_new_cases_smoothed_per_million=[],
                 mdv_total_cases_per_million=[],
-                mdv_new_cases=[],
                 npl_new_cases_smoothed_per_million=[],
                 npl_new_deaths_smoothed_per_million=[],
                 npl_total_cases_per_million=[],
-                npl_new_cases=[],
                 pak_new_cases_smoothed_per_million=[],
                 pak_new_deaths_smoothed_per_million=[],
-                pak_total_cases_per_million=[],
-                pak_new_cases=[]
+                pak_total_cases_per_million=[]
                 ;
 
               //labels (taken from MDV dataset)
@@ -102,85 +95,69 @@ export default class Home extends Component {
                   lka_new_cases_smoothed_per_million.push(element.new_cases_smoothed_per_million);
                   lka_new_deaths_smoothed_per_million.push(element.new_deaths_smoothed_per_million);
                   lka_total_cases_per_million.push(element.total_cases_per_million);
-                  lka_new_cases.push(element.new_cases);
                 });
               //afganistan
               sa_data_filtered[1].forEach(element => {
                 afg_new_cases_smoothed_per_million.push(element.new_cases_smoothed_per_million);
                 afg_new_deaths_smoothed_per_million.push(element.new_deaths_smoothed_per_million);
                 afg_total_cases_per_million.push(element.total_cases_per_million);
-                afg_new_cases.push(element.new_cases);
               });
               //bangladesh
               sa_data_filtered[2].forEach(element => {
                 bgd_new_cases_smoothed_per_million.push(element.new_cases_smoothed_per_million);
                 bgd_total_cases_per_million.push(element.total_cases_per_million);
-                bgd_new_cases.push(element.new_cases);
               });
               //bhutan
               sa_data_filtered[3].forEach(element => {
                 btn_new_cases_smoothed_per_million.push(element.new_cases_smoothed_per_million);
                 btn_total_cases_per_million.push(element.total_cases_per_million);
-                btn_new_cases.push(element.new_cases);
               });
               //ind
               sa_data_filtered[4].forEach(element => {
                 ind_new_cases_smoothed_per_million.push(element.new_cases_smoothed_per_million);
                 ind_new_deaths_smoothed_per_million.push(element.new_deaths_smoothed_per_million);
                 ind_total_cases_per_million.push(element.total_cases_per_million);
-                ind_new_cases.push(element.new_cases);
               });
               //mdv
               sa_data_filtered[5].forEach(element => {
                 mdv_new_cases_smoothed_per_million.push(element.new_cases_smoothed_per_million);
                 mdv_total_cases_per_million.push(element.total_cases_per_million);
-                mdv_new_cases.push(element.new_cases);
               });
               //npl
               sa_data_filtered[6].forEach(element => {
                 npl_new_cases_smoothed_per_million.push(element.new_cases_smoothed_per_million);
                 npl_new_deaths_smoothed_per_million.push(element.new_deaths_smoothed_per_million);
                 npl_total_cases_per_million.push(element.total_cases_per_million);
-                npl_new_cases.push(element.new_cases);
               });
               //pak
               sa_data_filtered[7].forEach(element => {
                 pak_new_cases_smoothed_per_million.push(element.new_cases_smoothed_per_million);
                 pak_new_deaths_smoothed_per_million.push(element.new_deaths_smoothed_per_million);
                 pak_total_cases_per_million.push(element.total_cases_per_million);
-                pak_new_cases.push(element.new_cases);
               });
               this.setState({
                   labels,
                   lka_new_cases_smoothed_per_million,
                   lka_new_deaths_smoothed_per_million,
                   lka_total_cases_per_million,
-                  lka_new_cases,
                   afg_new_cases_smoothed_per_million,
                   afg_new_deaths_smoothed_per_million,
                   afg_total_cases_per_million,
-                  afg_new_cases,
                   bgd_new_cases_smoothed_per_million,
                   bgd_total_cases_per_million,
-                  bgd_new_cases,
                   btn_new_cases_smoothed_per_million,
                   btn_total_cases_per_million,
-                  btn_new_cases,
                   ind_new_cases_smoothed_per_million,
                   ind_new_deaths_smoothed_per_million,
                   ind_total_cases_per_million,
-                  ind_new_cases,
                   mdv_new_cases_smoothed_per_million,
                   mdv_total_cases_per_million,
-                  mdv_new_cases,
                   npl_new_cases_smoothed_per_million,
                   npl_new_deaths_smoothed_per_million,
                   npl_total_cases_per_million,
-                  npl_new_cases,
                   pak_new_cases_smoothed_per_million,
                   pak_new_deaths_smoothed_per_million,
-                  pak_total_cases_per_million,
-                  pak_new_cases
+                  pak_total_cases_per_million
               }
               )
             this.setState({
@@ -240,7 +217,6 @@ export default class Home extends Component {
       </div>
   </div>
   
- 
   {(this.state.nav==='nc')?<NCPMChart className="chartcontainer" data={this.state}/>:null}
   {(this.state.nav==='tc')?<TCPMChart className="chartcontainer" data={this.state}/>:null}
   {(this.state.nav==='nd')?<NDPMChart className="chartcontainer" data={this.state}/>:null}
